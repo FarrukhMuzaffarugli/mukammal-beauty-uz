0f5f151bd2227c05f1787b24dea7da7d4f0bd8d5
FarrukhMuzaffarugli <mfarrukhbekn@gmail.com>
2025-11-04 13:53:39 +0900

    chore: initial scaffold (backend/mobile/admin) for Cursor Agent

---
+++ admin/src/main.tsx
@@ -0,0 +1,4 @@
+﻿import React from 'react';
+import { createRoot } from 'react-dom/client';
+const App = () => <div>BeautyUZ Admin</div>;
+createRoot(document.getElementById('root')!).render(<App />);
